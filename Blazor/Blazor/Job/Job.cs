166ad7a5200ca6f879cd6f3dd484413e975ea9f1
luclong <58335041+luclong@users.noreply.github.com>
2021-03-04 05:38:06 +0700

    3/3/2021

---
+++ Blazor/Blazor/Job/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Blazor.Job
+{
+    public partial class Job
+    {
+        public int IdJob { get; set; }
+        public string Namejob { get; set; }
+        public DateTime Starttime { get; set; }
+        public DateTime Endtime { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Blazor/Blazor/Job/TodoContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Blazor.Job
+{
+    public partial class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Job> Jobs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=Todo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Job>(entity =>
+            {
+                entity.HasKey(e => e.IdJob);
+
+                entity.ToTable("Job");
+
+                entity.Property(e => e.IdJob).HasColumnName("id_Job");
+
+                entity.Property(e => e.Endtime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("endtime");
+
+                entity.Property(e => e.Namejob).HasColumnName("namejob");
+
+                entity.Property(e => e.Starttime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("starttime");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
